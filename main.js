127f4815c351e466d67cf3e60e64f09503a16f1f
AtsushiSato <69712614+atushi0625@users.noreply.github.com>
2020-11-06 22:38:33 +0900

    ファーストコミット

---
+++ main.js
@@ -0,0 +1,97 @@
+'use strict';
+
+{
+    const question = document.getElementById('question');
+    const choices = document.getElementById('choices');
+    const btn = document.getElementById('btn');
+    const result = document.getElementById('result');
+    const scoreLabel = document.querySelector('#result >p');
+
+    const quizSet =  shuffle([
+        {q: '世界で一番大きい湖は？', c: ['カスピ海', 'カリブ海', '琵琶湖']},
+        {q: '２の８乗は？', c: ['256', '64', '1024']},
+        {q: '次のうち、最初にリリースされた言語は？', c: ['Python', 'Javascript', 'HTML']},
+        {q: '次のうち、最低賃金の都道府県は？', c: ['秋田県', '静岡県', '群馬県','岡山県']},
+    ]);
+
+    let currentNum = 0;
+    let isAnswered;
+    let score = 0;
+
+    
+    
+    function shuffle(arr){
+        for (let i = arr.length - 1; i>0; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[j], arr[i]] = [arr[i], arr[j]];
+        }
+        return arr;
+    }
+
+    function checkAnswer(li){
+        // if (isAnswered === true) {
+        if (isAnswered) {
+            return;
+        }
+        isAnswered = true;
+
+
+        if (li.textContent === quizSet[currentNum].c[0]){
+            li.classList.add('correct');
+            score++;
+            // console.log('correct');
+        } else {
+            li.classList.add('wrong');
+            // console.log('wrong');
+        }
+
+
+        btn.classList.remove('disabled');
+    }
+
+
+
+    function setQuiz() {
+        isAnswered = false;
+
+        question.textContent = quizSet[currentNum].q;
+        
+        while(choices.firstChild){
+            choices.removeChild(choices.firstChild);
+
+        }
+    
+        const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+        // console.log(quizSet[currentNum].c); 
+        shuffledChoices.forEach(choice =>{
+            const li = document.createElement('li');
+            li.textContent = choice;
+            li.addEventListener('click', () =>{
+              checkAnswer(li);
+            });
+            choices.appendChild(li);
+        });
+
+        if (currentNum === quizSet.length - 1){
+            btn.textContent = 'Show Score';
+        }
+       }
+
+       setQuiz();
+
+       btn.addEventListener('click', () =>{
+        if (btn.classList.contains('disabled')){
+            return;
+        }
+        btn.classList.contains('disabled');
+
+        if (currentNum === quizSet.length - 1){
+            // console.log(`Score: ${score} / ${quizSet.length}`);
+            scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+            result.classList.remove('hidden');
+        } else{
+            currentNum++;
+            setQuiz();
+            }
+         });
+    }
